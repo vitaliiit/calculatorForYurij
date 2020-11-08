b2b98a8747da75cbba8f424321134716eff02182
Vitaliiit <vv8307386@gmail.com>
2020-11-08 03:24:20 +0200

    Initial commit

---
+++ src/Central.java
@@ -0,0 +1,13 @@
+public class Central {
+    public static void main(String[] main) {
+
+        InsideOfCalculator insideOfCalculator = new InsideOfCalculator();
+//        CalculateService calculateService = new CalculateService();
+
+        insideOfCalculator.start();
+//        insideOfCalculator.enterNumber();
+//        insideOfCalculator.chooseOperator();
+//        calculateService.math();
+//        insideOfCalculator.result();
+    }
+}
---
+++ src/InsideOfCalculator.java
@@ -0,0 +1,98 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class InsideOfCalculator {
+
+    Scanner scanner = new Scanner(System.in);
+
+    private static int firstOperand;
+    private static int secondOperand;
+    private static String operator;
+    private static int result;
+
+    public int getFirstOperand() {
+        return firstOperand;
+    }
+
+    public void setFirstOperand(int firstOperand) {
+        this.firstOperand = firstOperand;
+    }
+
+    public int getSecondOperand() {
+        return secondOperand;
+    }
+
+    public void setSecondOperand(int secondOperand) {
+        this.secondOperand = secondOperand;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+
+    void start() {
+        System.out.println("\nYura hello my friend! I glad to see you in my amazing calculator world!\n");
+        enterNumber();
+    }
+
+    void enterNumber() {
+        System.out.println("Yura enter the first operand!");
+
+        try {
+            firstOperand = scanner.nextInt();
+            System.out.println("You enter - " + firstOperand);
+            System.out.println("Yura enter the second operand!");
+            secondOperand = scanner.nextInt();
+            System.out.println("You enter - " + secondOperand + "\n");
+            System.out.println("Thanks Yura!\n");
+            chooseOperator();
+        } catch (InputMismatchException e) {
+            System.out.println("Yura you enter too large number. Be careful!\n");
+        }
+    }
+
+    void chooseOperator() {
+        System.out.println("Yura what do you want todo of this operands?\n");
+        System.out.println("if you want 'added' this operands - enter 'add' or character '+'\n" +
+                "if you want 'subtracted' this operands - enter 'subtract' or character '-'\n" +
+                "if you want 'multiply' this operands - enter 'multiply' or character '*' \n" +
+                "if you want 'divided' this operands - enter 'divide' or character '/'");
+        scanner.nextLine();
+        operator = scanner.nextLine();
+        System.out.println("Yura you choose - " + operator + "\n");
+        CalculateService calculateService = new CalculateService();
+        calculateService.math();
+    }
+
+    void result() {
+        if (result == 545451687) {
+            System.out.println("Try again!\n");
+            enterNumber();
+        } else {
+            System.out.println("result is - " + result + "\n");
+            System.out.println("Nice work Yura!");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
